841859abd343a8406b9fa2cccc417e09e3356751
Helen Du <helendu.614@gmail.com>
2018-05-17 22:56:58 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)VALUES(1, "Mr. Chocolate", 5, "M", "brown", "easy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)VALUES(2, "Rowdy", 10, "M", "brown", "sad", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)VALUES(3, "Tabitha", 7, "F", "brown", "sassy", 1 );
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)VALUES(4, "Sergeant Brown", 11, "F", "black", "easy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)VALUES(5, "Melissa", 2, "M", "white", "easy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)VALUES(6, "Grinch", 3, "F", "black", "fire", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)VALUES(7, "Wendy", 5, "M", "brown", "sassy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)VALUES(8, NULL, 1, "M", "white", "easy", 1);
